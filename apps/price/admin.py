6d24f9b76379b75b0da0919e19a689b9e6cbc47d
sadra <sadraabadkar1383@gmail.com>
2026-05-25 17:19:00 +0330

    add price app

--- apps/price/admin.py
+++ apps/price/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ProductPriceHistory, ProductStockHistory
 
-# Register your models here.
+
+@admin.register(ProductPriceHistory)
+class ProductPriceHistoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'price_old', 'price_new', 'percent_change', 'change_type', 'created_at')
+    list_filter = ('change_type', 'source')
+    search_fields = ('product__title',)
+    readonly_fields = ('price_old', 'price_new', 'percent_change', 'change_type', 'created_at')
+
+
+@admin.register(ProductStockHistory)
+class ProductStockHistoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'stock_old', 'stock_new', 'change_type', 'created_at')
+    list_filter = ('change_type',)
+    search_fields = ('product__title',)
+    readonly_fields = ('stock_old', 'stock_new', 'change_type', 'created_at')
